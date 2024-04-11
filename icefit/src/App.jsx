119fe71b10479bdd684eec4d6416970a990b812f
Hristea Mircea <hristeamircea@yahoo.com>
2024-04-11 16:12:52 +0300

    Landing page before splitting into components

--- icefit/src/App.jsx
+++ icefit/src/App.jsx
@@ -31,26 +31,26 @@ function App() {
                                   </div>
                                 </div>
                               </nav>
-                              <section className="hero flex flex-col items-center justify-center h-full">
-                                <div className='grid grid-cols-1 md:grid-cols-2 gap-5 text-primary font-poppins px-32 mb-40'>
-                                    <div className="flex-col flex gap-5 justify-center">
+                              
+                                <div className='grid grid-cols-1 md:grid-cols-2 gap-5 text-primary font-poppins px-32 h-full justify-center items-center'>
+                                  <div className="flex-col flex gap-5 justify-center items-center md:items-start ">
                                   <div>
-                                      <span className="text-4xl">Empower Your</span>
+                                      <span className="text-2xl md:text-4xl text-center">Empower Your</span>
                                   <br/>
-                                  <span className='text-4xl'>Wellness Journey!</span>
+                                  <span className='text-2xl md:text-4xl text-center'>Wellness Journey!</span>
                                   </div>
                                   
-                                  <p>Join IceFit today and transform your life with personalized training,
+                                  <p className='text-center md:text-left'>Join IceFit today and transform your life with personalized training,
                                      nutrition guidance, and the support you need to achieve
                                       your healthiest self.</p>
-                                  <button className="bg-primary rounded text-white px-4 py-2 w-2/5 lg:px-7 lg:py-4 block">Get Started</button>
-                                </div>
-                                <div>
-                                  <img className='object-cover' src={IceFit} alt="IceFit Logo" />
-                                </div>
+                                  <button className="bg-primary rounded text-white px-4 py-2 lg:px-7 lg:py-4 ">Get Started</button>
+                                  </div>
+                                  <div>
+                                  <img className='flex justify-center' src={IceFit} alt="IceFit Logo" />
+                                  </div>
                                 </div>
                                 
-                              </section>
+                            
                             </div>
                   
     </>
